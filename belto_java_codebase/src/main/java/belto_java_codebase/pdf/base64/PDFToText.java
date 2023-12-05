98929fd0bbc165aa77764632a316672028dc22dc
Michael Johnson <109987452+metamike23@users.noreply.github.com>
2023-12-04 20:35:04 -0800

    Update PDFToText.java

--- belto_java_codebase/src/main/java/belto_java_codebase/pdf/base64/PDFToText.java
+++ belto_java_codebase/src/main/java/belto_java_codebase/pdf/base64/PDFToText.java
@@ -7,38 +7,42 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 
 public class PDFToText {
-	
+
+	// Instance variables
 	private String unstructured_text;
 	private String file_name;
 	private String source_file_path;
-	
+
+	// Constructor for PDFToText class with 1 String argument for fileName
 	public PDFToText(String fileName) {
 		
 		// Get the project directory
 		String projectDirectory = System.getProperty("user.dir"); 
 		// Set path to output folder
-        String targetDirectory = projectDirectory + File.separator + "target" + File.separator + "output"; 
+        	String targetDirectory = projectDirectory + File.separator + "target" + File.separator + "output"; 
         
-        
-        this.file_name = fileName;
-        this.source_file_path = targetDirectory + File.separator + fileName;
+        	this.file_name = fileName; // Set value for file_name 
+        	this.source_file_path = targetDirectory + File.separator + fileName;  // Set value for source_file_path
        
 	}
-	
+
+	// public method to set the value of the unstructured_text instance variable 
 	public void parseText() throws IOException {
 		String path = this.source_file_path;
 		this.unstructured_text = readPDFText(path);
 	}
-	
+
+	// private method to extract the text from a pdf file
 	private String readPDFText(String pdfFilePath) throws IOException {
 		try (PDDocument document = PDDocument.load(new java.io.File(pdfFilePath))) {
 			// Create a PDFTextStripper object
-	        PDFTextStripper textStripper = new PDFTextStripper();
-	        // Get the text content from the PDF document
-	        return textStripper.getText(document);
+	        	PDFTextStripper textStripper = new PDFTextStripper();
+	        	// Get the text content from the PDF document
+	        	return textStripper.getText(document);
 	    }
 	}
-	
+
+	// Getter methods
 	public String getUnstructuredText() {
 		return unstructured_text;
 	}
@@ -47,10 +51,6 @@ public class PDFToText {
 		return file_name;
 	}
 	
-	
-	
-	
-
 	
 	  /*
 
